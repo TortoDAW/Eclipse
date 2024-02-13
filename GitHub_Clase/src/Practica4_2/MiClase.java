8044a483f230105e540fb592714f477cc4504fb4
Tortousa <danielltortosa@gmail.com>
2024-02-13 22:43:17 +0100

    Nuevos archivos

---
+++ GitHub_Clase/src/Practica4_2/MiClase.java
@@ -0,0 +1,7 @@
+package Practica4_2;
+
+public class MiClase {
+    public static void main(String[] args) {
+        System.out.println("¡Hola, mundo desde MiClase!");
+    }
+}
---
+++ GitHub_Clase/src/Practica4_2/OtraClase.java
@@ -0,0 +1,8 @@
+package Practica4_2;
+
+public class OtraClase {
+    public static void main(String[] args) {
+        System.out.println("¡Hola, mundo desde OtraClase!");
+    }
+}
+
